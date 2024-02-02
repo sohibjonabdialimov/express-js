3ab10667f4c329c3604f1c89247a5752554a6376
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-02-02 21:26:11 +0500

    express js

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {create} from "express-handlebars";
+const app = express();
+
+const hbs = create({
+  defaultLayout: 'main',
+  extname: "hbs"
+})
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+app.get("/about", (req, res) => {
+  res.render("about")
+})
+
+const PORT = process.env.PORT || 4100;
+app.listen(PORT, () => console.log("Server is running on " + PORT));
+// console.log(process.env.PORT);
